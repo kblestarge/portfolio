9e65daf77967c29a91c964d7a97a4a2f493f02ec
klestarg <kblestarge@gmail.com>
2015-06-11 21:57:25 -0600

    Home page styling almost finished

--- portfolio.js
+++ portfolio.js
@@ -4,22 +4,28 @@ var main = function() {
 	console.log("Hello world");
 
 	$('#kbtn').click(function() {
-            $('#main h1').toggleClass("appear");
+            $('#main h1').toggleClass("appear slide");
             $('#main .main-btn').toggleClass("appear");
-            $('#proj-menu').removeClass("appear");
-            $('#soc-menu').removeClass("appear");
-
-            $(this).toggleClass("pressed");
+            $('#proj-menu').removeClass("appear fade");
+            $('#soc-menu').removeClass("appear fade");
+            $('#projects').removeClass("main-btnPressed");
+            $('#socialM').removeClass("main-btnPressed");
+            //add http://css3.bradshawenterprises.com/cfimg/
+            $(this).toggleClass("kbtnPressed");
     });
 
 	$('#main #projects').click(function() {
-            $('#proj-menu').toggleClass("appear");
-            $('#soc-menu').removeClass("appear");
+            $('#proj-menu').toggleClass("appear fade");
+            $(this).toggleClass("main-btnPressed");
+            $('#soc-menu').removeClass("appear fade");
+            $('#socialM').removeClass("main-btnPressed");
     });
 
 	$('#main #socialM').click(function() {
-            $('#soc-menu').toggleClass("appear");
-            $('#proj-menu').removeClass("appear");
+            $('#soc-menu').toggleClass("appear fade");
+            $(this).toggleClass("main-btnPressed");
+            $('#proj-menu').removeClass("appear fade");
+            $('#projects').removeClass("main-btnPressed");
     });
 
 }
