006550a87a3759484b9c6100bd2a488eb7e10c2f
Kevin LeStarge <kblestarge@gmail.com>
2015-06-12 16:43:50 -0600

    web proj page styled

--- portfolio.js
+++ portfolio.js
@@ -3,31 +3,45 @@ var main = function() {
 
 	console.log("Hello world");
 
-	$('#kbtn').click(function() {
-            $('#main h1').toggleClass("appear slide");
-            $('#main .main-btn').toggleClass("appear");
+	$('.kbtn').click(function() {
+            $('#home h1').toggleClass("appear slide");
+            $('#home .main-btn').toggleClass("appear");
+
             $('#proj-menu').removeClass("appear fade");
             $('#soc-menu').removeClass("appear fade");
             $('#projects').removeClass("main-btnPressed");
             $('#socialM').removeClass("main-btnPressed");
-            //add http://css3.bradshawenterprises.com/cfimg/
+
             $(this).toggleClass("kbtnPressed");
     });
 
-	$('#main #projects').click(function() {
+	$('#home #projects').click(function() {
             $('#proj-menu').toggleClass("appear fade");
             $(this).toggleClass("main-btnPressed");
             $('#soc-menu').removeClass("appear fade");
             $('#socialM').removeClass("main-btnPressed");
     });
 
-	$('#main #socialM').click(function() {
+	$('#home #socialM').click(function() {
             $('#soc-menu').toggleClass("appear fade");
             $(this).toggleClass("main-btnPressed");
             $('#proj-menu').removeClass("appear fade");
             $('#projects').removeClass("main-btnPressed");
     });
 
+
+	//Web Projects
+    $('#madlib').hover(function() {
+    		$('#madlib span').text('');},
+    		function() {
+    		$('#madlib span').text('Mad Lib');
+	});
+
+    $('#fizzbuzz').hover(function() {
+    		$('#fizzbuzz span').text('');},
+    		function() {
+    		$('#fizzbuzz span').text('Fizzbuzz');
+	});
 }
 
 $(document).ready(main);
