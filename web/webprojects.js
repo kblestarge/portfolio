7790ad16f00e36ecfbea1692c7b9cad4dd7468d0
klestarg <kblestarge@gmail.com>
2015-08-13 21:03:28 -0600

    Agularizing

--- web/webprojects.js
+++ web/webprojects.js
@@ -4,81 +4,59 @@ var app = angular.module('portfolio', []);
 app.directive('webproj', function(){
     return {
         restrict: 'E',
-        templateUrl: 'vid-proj.html',
+        templateUrl: 'web-proj.html',
         controller: function() {
-            this.projects = [
+
+            var self = this;
+            self.hidden = null;
+
+            self.projects = [
                 {
-                    class : 'youtube',
-                    src : 'https://www.youtube.com/embed/R1wM3lmLufI',
-                    title: 'The Kord Lord',
-                    description: '30 Second Spot',
-                    camera: 'Sony a6000',
-                    pub_date: new Date('November, 2014'),
-                    role: 'All Roles'
+                    href : 'http://worldtechacademystudents.com/klestarge/fizzbuzz/',
+                    id : 'fizzbuzz',
+                    title: 'FizzBuzz'
                 },
                 {
-                    class : 'youtube',
-                    src : 'https://www.youtube.com/embed/gfRkMKyC4R0',
-                    title: 'Songs from the Wood',
-                    description: 'Short Narrative: $0 Budget',
-                    camera: 'Panasonic AG-HMC150',
-                    pub_date: new Date('October 2009'),
-                    role: 'Codirector/Coproducer'
+                    href : 'http://worldtechacademystudents.com/klestarge/madlib/',
+                    id : 'madlib',
+                    title: 'Madlib'
                 },
                 {
-                    class : 'vimeo',
-                    src : 'https://player.vimeo.com/video/49177543?title=0&byline=0&portrait=0',
-                    title: 'The Antagonist',
-                    description: 'Short Narrative: $5,000 Budget',
-                    camera: 'Red One',
-                    pub_date: new Date('August 2009'),
-                    role: 'Codirector/Coproducer'
+                    href : 'http://worldtechacademystudents.com/klestarge/tictactoe/',
+                    id : 'tictactoe',
+                    title: 'TicTacToe'
                 },
                 {
-                    class : 'vimeo',
-                    src : 'https://player.vimeo.com/video/128838712',
-                    title: 'Ashley & Sae',
-                    description: 'Wedding Highlight Reel',
-                    camera: 'Sony SF7',
-                    pub_date: new Date('June 2015'),
-                    role: 'Editor'
+                    href : 'http://worldtechacademystudents.com/klestarge/calculator/',
+                    id : 'calculator',
+                    title: 'Calculator'
                 },
                 {
-                    class : 'youtube',
-                    src : 'https://www.youtube.com/embed/Fk20VdL-LYI',
-                    title: 'Paul Cardall: Teagan\'s Story',
-                    description: '3 Minute Viral Video',
-                    camera: 'Sony SF7, Sony a72',
-                    pub_date: new Date('December 2014'),
-                    role: '2nd Camera, Editor'
+                    href : 'http://worldtechacademystudents.com/klestarge/photogallery/',
+                    id : 'photogallery',
+                    title: 'PhotoGallery'
+                },
+                {
+                    href : 'http://worldtechacademystudents.com/klestarge/hybridvsnormalcar/',
+                    id : 'hybrid',
+                    title: 'CarFighter'
+                },
+                {
+                    href : 'http://worldtechacademystudents.com/klestarge/snake/',
+                    id : 'snake',
+                    title: 'Snake'
                 }
             ];
+
+            self.eraseTitle = function(id) {
+                self.hidden = id;
+            };
+
+            self.resetTitle = function() {
+                self.hidden = null;
+            };
 
         },
         controllerAs: "web"
     };
 });
-
-var main = function() {
-
-	//Web Projects
-    $('#madlib').hover(function() {
-    		$('#madlib span').text('');},
-    		function() {
-    		$('#madlib span').text('Mad Lib');
-	});
-
-    $('#fizzbuzz').hover(function() {
-    		$('#fizzbuzz span').text('');},
-    		function() {
-    		$('#fizzbuzz span').text('Fizzbuzz');
-	});
-
-    $('#chf').hover(function() {
-            $('#chf span').text('');},
-            function() {
-            $('#chf span').text('CHF');
-    });
-};
-
-$(document).ready(main);
